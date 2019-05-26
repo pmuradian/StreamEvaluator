326722146d760d72b7fff6411f35bf01d644a518
pmuradian <p.muradian@gmail.com>
2019-05-26 21:46:02 +0200

    Write to hdfs

--- src/main/java/exercises/StreamEvaluator.java
+++ src/main/java/exercises/StreamEvaluator.java
@@ -19,33 +19,75 @@ import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.apache.kafka.common.serialization.LongSerializer;
 
-//import org.apache.hadoop;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FSDataOutputStream;
 import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.hdfs.DistributedFileSystem;
 
+import java.io.BufferedOutputStream;
+import java.io.BufferedWriter;
 import java.io.IOException;
+import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Random;
 
-class Hasher extends DoFn< KV<String, String>, KV<String, String> > {
-    @ProcessElement
-    public void processElement(@Element KV<String, String> kv, OutputReceiver<KV<String, String>> out) {
-        System.out.println(kv.getKey() + "*****" + kv.getValue());
-        Integer key = this.hash();
-        if (key < 3) {
-            StreamEvaluator.buckets.get(this.hash()).add(kv);
-
-        }
-        out.output(kv);
-    }
-
-    private Integer hash() {
-        return (new Random()).nextInt(StreamEvaluator.N);
-    }
-}
+//class Hasher extends DoFn< KV<String, String>, KV<String, String> > {
+//    @ProcessElement
+//    public void processElement(@Element KV<String, String> kv, OutputReceiver<KV<String, String>> out) {
+//        System.out.println(kv.getKey() + "*****" + kv.getValue());
+//        Integer key = this.hash();
+//        if (key < 3) {
+//            StreamEvaluator.buckets.get(this.hash()).add(kv);
+////            HDFSWriter.writeTo("/bvpu.txt", kv);
+//            System.out.println("******************* " + kv.getKey());
+//        }
+//        out.output(kv);
+//    }
+//
+//    private Integer hash() {
+//        return (new Random()).nextInt(StreamEvaluator.N);
+//    }
+//}
 
 class HDFSWriter {
-    boolean writeTo(String path) {
+
+    final static String hdfsPath = "hdfs://localhost:8020";
+    final static String hdfsName = "fs.defaultFS";
+
+    static boolean writeTo(String pathString, String content) {
+
+        Configuration conf = new Configuration();
+//        conf.addResource(new Path("file://home/azazel/Documents/hadoop/hadoop-2.8.3/etc/hadoop/core-site.xml"));
+//        conf.addResource(new Path("file://home/azazel/Documents/hadoop/hadoop-2.8.3/etc/hadoop/hdfs-site.xml"));
+        conf.set("fs.hdfs.impl", org.apache.hadoop.hdfs.DistributedFileSystem.class.getName());
+//        conf.set("fs.")
+        conf.set(hdfsName, hdfsPath);
+        System.out.println(conf.toString());
+        try {
+            Path path = new Path(pathString);
+
+            FileSystem fs = FileSystem.get(conf);
+            if (!fs.exists(path)) {
+                System.out.println("writing bvpu");
+                FSDataOutputStream outputStream = fs.create(path);
+                BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(outputStream, "UTF-8"));
+                writer.write(content);
+                writer.close();
+                fs.copyFromLocalFile(path, new Path("hdfs://localhost:8020/user/azazel/input/bvpu.txt"));
+                fs.close();
+                System.out.println("bvpu written");
+            } else {
+                System.out.println("file exists");
+            }
+        }
+        catch (Exception e) {
+
+
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        }
         return true;
     }
 }
@@ -53,19 +95,19 @@ class HDFSWriter {
 public class StreamEvaluator {
     // Read streamed data from Kafka and save it to HDFS
 
-    public static HashMap<Integer, ArrayList<KV<String, String>>> buckets = new HashMap<>();
-    public static final Integer N = 100;
-    private static final Integer samplePercentage = 3;
+//    public static HashMap<Integer, ArrayList<KV<String, String>>> buckets = new HashMap<>();
+//    public static final Integer N = 100;
+//    private static final Integer samplePercentage = 3;
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         // Implement a sampling algorithm which keeps a random sample of size N of elements seen up to this points.
         // Note that each element should have the same probability of being in the sample. After seeing M elements it should be min(N/M, 1)
-        PipelineOptions pipelineOptions = PipelineOptionsFactory.create();
-        Pipeline pipeline = Pipeline.create(pipelineOptions);
-
-        for (int i = 0; i < samplePercentage; i++) {
-            buckets.put(i, new ArrayList<>());
-        }
+//        PipelineOptions pipelineOptions = PipelineOptionsFactory.create();
+//        Pipeline pipeline = Pipeline.create(pipelineOptions);
+//
+//        for (int i = 0; i < samplePercentage; i++) {
+//            buckets.put(i, new ArrayList<>());
+//        }
 
 //        pipeline.apply(kafkaReader())
 //                .apply(doubleValue())
@@ -78,23 +120,27 @@ public class StreamEvaluator {
 //                        }))
 //                .apply(kafkaWriter());
 
-        pipeline.apply(kafkaReader())
-                .apply(ParDo.of(new Hasher()));
+//        pipeline.apply(kafkaReader())
+//                .apply(ParDo.of(new Hasher()));
 
-        PipelineResult result = pipeline.run();
-        try {
-            result.waitUntilFinish();
-        } catch (Exception exc) {
-            result.cancel();
-        }
+        System.out.println("writing to hdfs");
+        HDFSWriter.writeTo("hdfs://localhost:8020/user/azazel/input/bvpu.txt", "bvpu");
+        System.out.println("write completed");
+
+//        PipelineResult result = pipeline.run();
+//        try {
+//            result.waitUntilFinish();
+//        } catch (Exception exc) {
+//            result.cancel();
+//        }
     }
 
-    private static PTransform<PBegin,PCollection<KV<String, String>>> kafkaReader() {
-        return KafkaIO.<String, String>read()
-                .withBootstrapServers("localhost:9092")
-                .withTopic("demo-kv")
-                .withKeyDeserializer(StringDeserializer.class)
-                .withValueDeserializer(StringDeserializer.class)
-                .withoutMetadata();
-    }
+//    private static PTransform<PBegin,PCollection<KV<String, String>>> kafkaReader() {
+//        return KafkaIO.<String, String>read()
+//                .withBootstrapServers("localhost:9092")
+//                .withTopic("demo-kv")
+//                .withKeyDeserializer(StringDeserializer.class)
+//                .withValueDeserializer(StringDeserializer.class)
+//                .withoutMetadata();
+//    }
 }
